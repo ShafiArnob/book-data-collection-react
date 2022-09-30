8a9e001942de7722ef6536c50471fb709bd52319
ShafiArnob <shafiarnob@gmail.com>
2022-09-30 16:22:39 +0600

    post data to json server

--- src/pages/create/CreateForm.jsx
+++ src/pages/create/CreateForm.jsx
@@ -26,18 +26,58 @@ function CreateForm() {
     str = str.replace(/[a-zA-Z]/," ")
     return str
   }
+  const removeNewLineNotBookText = (str) =>{
+    str = str.toString()
+    str = str.replace(/\r?\n|\r/g, "")
+    // str = str.replace(/\r?\d|\r/g, "")
+    str = str.replace(/[.,/#!$%^*=\-_`~()'"॥€]/g,"");
+    // str = str.replace(/[a-zA-Z]/," ")
+    return str
+  }
 
   const handleSubmit = (e) =>{
     e.preventDefault()
+    const newBookTitle=removeNewLineNotBookText(title)
+    const newBookAbstract = removeNewline(abstract)
     const newBookText = removeNewline(bookText)
     // console.log({who,author,gender,birth,birthPlace,death,deathPlace});
     // console.log({title,publisher,publishDate,genre,newBookText});
-    console.log(newBookText);
+    // console.log(newBookText);
+
+    const data ={
+      who:who,
+      author:author,
+      gender:gender,
+      birth:birth,
+      birth_place:birthPlace,
+      death:death,
+      death_place:deathPlace,
+      title:newBookTitle,
+      publisher:publisher,
+      publish_data:publishDate,
+      genre:genre,
+      book_abstract:newBookAbstract,
+      book_text:newBookText
+    }
+
+    postData(data)
+  }
+  const postData = (data) =>{
+    fetch('http://localhost:8000/books', {
+      method: 'POST', // or 'PUT'
+      headers: {'Content-Type': 'application/json',},
+      body: JSON.stringify(data),
+    })
+    .then((response) => response.json())
+    .then((data) => {console.log('Success:', data);})
+    .catch((error) => {
+      console.error('Error:', error);
+    });
   }
 
   return (
     <div className="form-container">
-      <form onClick={handleSubmit} className='bg-slate-200'>
+      <form onSubmit={handleSubmit} className='bg-slate-200'>
         <label>
           <span>who:</span>
           <input type="text" onChange={(e) => setWho(e.target.value)} required/>
@@ -110,7 +150,7 @@ function CreateForm() {
         <textarea className='w-full' name="Book text" cols="30" rows="10"  onChange={(e) => setBookText(e.target.value)} required></textarea>
       </label>
 
-        <button class="button">Button</button>
+        <button type='submit' class="button">Submit</button>
       </form>
     </div>
   )
