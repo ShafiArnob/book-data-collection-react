4f8c864b2070ee5f1cb7e0961a794a901fe937f4
ShafiArnob <shafiarnob@gmail.com>
2022-10-03 19:27:03 +0600

    fetch single book data

--- src/pages/singleBook/SingleBook.jsx
+++ src/pages/singleBook/SingleBook.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 function SingleBook() {
   const {id} = useParams()
+  const [book, setBook] = useState({})
+  useEffect(()=>{
+    fetch(`http://localhost:8000/books/${id}`)
+    .then(res => res.json())
+    .then(data => setBook(data))
+  },[id])
+
   return (
     <div>
       <h1>SingleBook</h1>
